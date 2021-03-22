3e4b17ae5f3dd2e24248e4b5066307b2088bd27e
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-03-22 14:29:14 -0300

    6 - Assert

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -32,7 +32,7 @@ public class LocacaoServiceTest {
          */
 
         //automática
-        Assert.assertTrue(locacao.getValor() == 5.0);
+        Assert.assertEquals(5.0, locacao.getValor(), 0.01);
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
     }

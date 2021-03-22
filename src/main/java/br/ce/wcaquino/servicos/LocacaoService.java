089ded461e2c42eeee8dd29857cbd8161f568e7e
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-03-22 13:48:11 -0300

    5 - Ogranização dos arquivos de teste

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -1,15 +1,12 @@
 package br.ce.wcaquino.servicos;
 
-import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
-
-import java.util.Date;
-
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
-import org.junit.Assert;
-import org.junit.Test;
+
+import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 public class LocacaoService {
 	
@@ -31,26 +28,4 @@ public class LocacaoService {
 		return locacao;
 	}
 
-    @Test
-	public void teste() {
-		//cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
-		
-		//acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//verificacao
-
-		//Manual
-		System.out.println(locacao.getValor() == 4.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-
-		//automática
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-	}
 }
